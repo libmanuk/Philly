2c0df7ae45f59917153ac94e89d2b38314de7869
Eric C. Weig <eweig@email.uky.edu>
2017-01-31 17:14:54 -0500

    Add files via upload

---
+++ custom.php
@@ -0,0 +1,21 @@
+<?php
+function link_to_related_exhibits($item) {
+
+    $db = get_db();
+
+    $select = "
+    SELECT e.* FROM {$db->prefix}exhibits AS e
+    INNER JOIN {$db->prefix}exhibit_pages AS ep on ep.exhibit_id = e.id
+    INNER JOIN {$db->prefix}exhibit_page_blocks AS epb ON epb.page_id = ep.id
+    INNER JOIN {$db->prefix}exhibit_block_attachments AS epba ON epba.block_id = epb.id
+    WHERE epba.item_id = ?";
+
+$exhibits = $db->getTable("Exhibit")->fetchObjects($select,array($item->id));
+
+if(!empty($exhibits)) {
+    foreach($exhibits as $exhibit) {
+echo '<div style="float:right;"><h3><a href="'.exhibit_builder_exhibit_uri($exhibit).'">Biography</a></h3></div>';
+    }
+}
+}
+
