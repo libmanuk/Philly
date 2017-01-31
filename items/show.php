a599081e9c4f88c3cc31bcc1610bba27c3fa347c
Eric C. Weig <eweig@email.uky.edu>
2017-01-31 17:19:14 -0500

    Add files via upload

---
+++ items/show.php
@@ -0,0 +1,138 @@
+<?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
+<div id="primary">
+<div id="container">
+    <h1><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
+<?php echo link_to_related_exhibits($item); ?>
+<!--<div style="float:right;"><h3><a href="<?php echo WEB_ROOT; ?>/exhibits/show/<?php echo link_to_related_exhibits($item); ?>">Biography</a></h3></div>-->
+    <!-- The following checks to see if the item is of the Oral History item type and sets the display based on that. -->
+
+<?php 
+$type = $item->getItemType()->name;
+if ('Oral History' === $type): ?>
+
+
+<ul class="tab">
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">Interview</a></li>
+  <li><a href="javascript:void(0)" class="tablinks" onclick="openCity(event, 'Paris')">Description</a></li>
+</ul>
+
+
+
+<div id="London" class="tabcontenttoggle">
+
+
+        <?php echo metadata('item', array('Item Type Metadata', 'OHMS Object')); ?>
+
+
+</div>
+
+<div id="Paris" class="tabcontenttoggle">
+
+    <!-- Items metadata -->
+    <div id="item-metadata">
+        <?php echo all_element_texts('item'); ?>
+    </div>
+
+    <h3><?php echo __('Files'); ?></h3>
+    <div id="item-images">
+         <?php echo files_for_item(); ?>
+    </div>
+
+   <?php if(metadata('item','Collection Name')): ?>
+      <div id="collection" class="element">
+        <h3><?php echo __('Collection'); ?></h3>
+        <div class="element-text"><?php echo link_to_collection_for_item(); ?></div>
+      </div>
+   <?php endif; ?>
+
+     <!-- The following prints a list of all tags associated with the item -->
+    <?php if (metadata('item','has tags')): ?>
+    <div id="item-tags" class="element">
+        <h3><?php echo __('Tags'); ?></h3>
+        <div class="element-text"><?php echo tag_string('item'); ?></div>
+    </div>
+    <?php endif; ?>
+</div>
+
+
+
+</div>
+
+</div>
+
+
+    <!-- The following prints a citation for this item. -->
+    <div id="item-citation" class="element">
+        <h3><?php echo __('Citation'); ?></h3>
+        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
+    </div>
+       <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+
+
+    <ul class="item-pagination navigation">
+        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+    </ul>
+
+</div> <!-- End of Primary. -->
+
+<?php else: ?>
+
+    <!-- Items metadata -->
+    <div id="item-metadata">
+        <?php echo all_element_texts('item'); ?>
+    </div>
+
+    <h3><?php echo __('Files'); ?></h3>
+    <div id="item-images">
+         <?php echo files_for_item(); ?>
+    </div>
+
+   <?php if(metadata('item','Collection Name')): ?>
+      <div id="collection" class="element">
+        <h3><?php echo __('Collection'); ?></h3>
+        <div class="element-text"><?php echo link_to_collection_for_item(); ?></div>
+      </div>
+   <?php endif; ?>
+
+     <!-- The following prints a list of all tags associated with the item -->
+    <?php if (metadata('item','has tags')): ?>
+    <div id="item-tags" class="element">
+        <h3><?php echo __('Tags'); ?></h3>
+        <div class="element-text"><?php echo tag_string('item'); ?></div>
+    </div>
+    <?php endif; ?>
+
+
+</div>
+
+
+
+
+
+    <!-- The following prints a citation for this item. -->
+    <div id="item-citation" class="element">
+        <h3><?php echo __('Citation'); ?></h3>
+        <div class="element-text"><?php echo metadata('item','citation',array('no_escape'=>true)); ?></div>
+    </div>
+       <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+
+
+    <ul class="item-pagination navigation">
+        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+    </ul>
+
+</div> <!-- End of Primary. -->
+
+<?php endif; ?>
+
+<script>
+window.onload = function() {
+  document.getElementById("query").focus();
+}
+</script>
+
+ <?php echo foot(); ?>
+
+
