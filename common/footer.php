5bcae0b4c97d4efc71429adac1598f4508fc1e1f
Eric C. Weig <eweig@email.uky.edu>
2017-01-31 17:17:13 -0500

    Add files via upload

---
+++ common/footer.php
@@ -0,0 +1,54 @@
+</div><!-- end content -->
+
+<footer role="contentinfo">
+
+    <div id="footer-content" class="center-div">
+        <?php if($footerText = get_theme_option('Footer Text')): ?>
+        <div id="custom-footer-text">
+            <p><?php echo get_theme_option('Footer Text'); ?></p>
+        </div>
+        <?php endif; ?>
+        <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
+        <p><?php echo $copyright; ?></p>
+        <?php endif; ?>
+        <nav><?php echo public_nav_main()->setMaxDepth(0); ?></nav>
+        <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
+
+    </div><!-- end footer-content -->
+
+     <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
+
+</footer>
+
+
+<script>
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontenttoggle");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " activesplash";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
+
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        Omeka.showAdvancedForm();
+        Omeka.skipNav();
+        Omeka.megaMenu();
+        Berlin.dropDown();
+    });
+</script>
+
+</body>
+
+</html>
